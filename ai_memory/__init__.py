68cb2f02e2a492a09f57f22222fd373f09e1f890
BlakeFelix <79122900+BlakeFelix@users.noreply.github.com>
2025-07-29 10:07:35 -0400

    Lazy-load VectorMemory import

--- ai_memory/__init__.py
+++ ai_memory/__init__.py
@@ -1,3 +1,23 @@
-from .vector_memory import VectorMemory
+"""ai_memory package exposing VectorMemory lazily."""
 
 __all__ = ["VectorMemory"]
+
+
+class _VectorMemoryProxy:
+    def __call__(self, *args, **kwargs):
+        from .vector_memory import VectorMemory
+        return VectorMemory(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        from .vector_memory import VectorMemory
+        return getattr(VectorMemory, attr)
+
+
+_proxy = _VectorMemoryProxy()
+
+
+def __getattr__(name):
+    if name == "VectorMemory":
+        return _proxy
+    raise AttributeError(name)
+
